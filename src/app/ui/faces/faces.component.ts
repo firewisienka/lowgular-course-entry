1f62a5d67d16036bdfae9591117da89479bd25da
firewisienka <izabela.wisienka@gmail.com>
2022-11-18 13:48:06 +0100

    feat:  add services to employee-list and faces

--- src/app/ui/faces/faces.component.ts
+++ src/app/ui/faces/faces.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EmployeeService } from '../../services/employee.service';
 import { EmployeeModel } from '../../model/employee.model';
